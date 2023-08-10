a505bcd091d9dc638916cb88ec3a5f1ce51ba7f6
alhafidzhamdan <53702866+alhafidzhamdan@users.noreply.github.com>
2023-08-10 14:09:07 +0100

    ad

--- scripts/hmf/submit_gridss.sh
+++ scripts/hmf/submit_gridss.sh
@@ -26,7 +26,7 @@ PATIENT_ID=`head -n $SGE_TASK_ID $IDS | tail -n 1`
 
 source $CONFIG
 
-GRIDSS_ASSEMBLY=/exports/igmm/eddie/Glioblastoma-WGS/WGS/variants/sv/gridss/results/${PATIENT_ID}{$TYPE}.assembly.bam
+GRIDSS_ASSEMBLY=/exports/igmm/eddie/Glioblastoma-WGS/WGS/variants/sv/gridss/results/
 
 $GRIDSS \
     --reference $REFERENCE \
