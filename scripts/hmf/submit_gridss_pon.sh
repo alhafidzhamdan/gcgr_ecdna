3d7bead7f56c18e4c29adf8390dcee40f4b417a8
alhafidzhamdan <53702866+alhafidzhamdan@users.noreply.github.com>
2023-08-10 18:52:33 +0100

    a

--- scripts/hmf/submit_gridss_pon.sh
+++ scripts/hmf/submit_gridss_pon.sh
@@ -14,21 +14,22 @@
 ### VCF files also need to be decompressed
 
 CONFIG=$1
-BATCH=$2
+STAGE=$2
+BATCH=$3
 
 source $CONFIG
 
 java -Xmx8g -cp $GRIDSS_JAR gridss.GeneratePonBedpe \
 	$(ls -1 $GRIDSS_OUTPUT/GCGR*.gridss.raw.vcf | awk ' { print "INPUT=" $0 }') \
-	O=$GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.raw.bedpe \
-	SBO=$GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.raw.bed \
+	O=$GRIDSS_PON/gridss_pon_breakpoint.raw.bedpe \
+	SBO=$GRIDSS_PON/gridss_pon_single_breakend.raw.bed \
 	THREADS=16 \
 	NORMAL_ORDINAL=0 \
 	REFERENCE_SEQUENCE=$REFERENCE
 	
-	cat $GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.raw.bed | awk '$5>=3' > $GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.bed
-    cat $GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.raw.bedpe | awk '$8>=3' > $GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.bedpe
-    rm $GRIDSS_PON/${BATCH}/*raw*
+	cat $GRIDSS_PON/gridss_pon_single_breakend.raw.bed | awk '$5>=3' > $GRIDSS_PON/gridss_pon_single_breakend.bed
+    cat $GRIDSS_PON/gridss_pon_breakpoint.raw.bedpe | awk '$8>=3' > $GRIDSS_PON/gridss_pon_breakpoint.bedpe
+    rm $GRIDSS_PON/*raw*
     
 
 
