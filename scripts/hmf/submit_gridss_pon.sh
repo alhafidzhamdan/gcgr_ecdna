ab730e2360d6fd27a9eb2a7133dcfb43ca0cf231
alhafidzhamdan <53702866+alhafidzhamdan@users.noreply.github.com>
2023-08-10 14:37:06 +0100

    ad

--- scripts/hmf/submit_gridss_pon.sh
+++ scripts/hmf/submit_gridss_pon.sh
@@ -18,51 +18,18 @@ BATCH=$2
 
 source $CONFIG
 
-if [ ! -d $GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.bedpe ]
-then
-    if [ $BATCH == GCGR ]
-    then
-        java -Xmx8g \
-        	-cp $GRIDSS_JAR \
-        	gridss.GeneratePonBedpe \
-        	$(ls -1 $GRIDSS_OUTPUT/E*.gridss.raw.vcf | awk ' { print "INPUT=" $0 }') \
-        	O=$GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.raw.bedpe \
-        	SBO=$GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.raw.bed \
-        	THREADS=12 \
-        	NORMAL_ORDINAL=0 \
-        	REFERENCE_SEQUENCE=$REFERENCE
-        cat $GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.raw.bed | awk '$5>=3' > $GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.bed
-        cat $GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.raw.bedpe | awk '$8>=3' > $GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.bedpe
-        rm $GRIDSS_PON/${BATCH}/*raw*
-    elif [ $BATCH == DBGAP_TCGA ]
-    then 
-        java -Xmx8g \
-        	-cp $GRIDSS_JAR \
-        	gridss.GeneratePonBedpe \
-        	$(ls -1 $GRIDSS_OUTPUT/DO*.gridss.raw.vcf | awk ' { print "INPUT=" $0 }') \
-        	O=$GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.raw.bedpe \
-        	SBO=$GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.raw.bed \
-        	THREADS=12 \
-        	NORMAL_ORDINAL=0 \
-        	REFERENCE_SEQUENCE=$REFERENCE
-        cat $GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.raw.bed | awk '$5>=3' > $GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.bed
-        cat $GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.raw.bedpe | awk '$8>=3' > $GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.bedpe
-        rm $GRIDSS_PON/${BATCH}/*raw*
-    else
-        java -Xmx8g \
-        	-cp $GRIDSS_JAR \
-        	gridss.GeneratePonBedpe \
-        	$(ls -1 $GRIDSS_OUTPUT/${BATCH}/${BATCH}*.gridss.raw.vcf | awk ' { print "INPUT=" $0 }') \
-        	O=$GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.raw.bedpe \
-        	SBO=$GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.raw.bed \
-        	THREADS=12 \
-        	NORMAL_ORDINAL=0 \
-        	REFERENCE_SEQUENCE=$REFERENCE
-        cat $GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.raw.bed | awk '$5>=3' > $GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.bed
-        cat $GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.raw.bedpe | awk '$8>=3' > $GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.bedpe
-        rm $GRIDSS_PON/${BATCH}/*raw*
-    fi
-fi
+java -Xmx8g -cp $GRIDSS_JAR gridss.GeneratePonBedpe \
+	$(ls -1 $GRIDSS_OUTPUT/GCGR*.gridss.raw.vcf | awk ' { print "INPUT=" $0 }') \
+	O=$GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.raw.bedpe \
+	SBO=$GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.raw.bed \
+	THREADS=16 \
+	NORMAL_ORDINAL=0 \
+	REFERENCE_SEQUENCE=$REFERENCE
+	
+	cat $GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.raw.bed | awk '$5>=3' > $GRIDSS_PON/${BATCH}/gridss_pon_single_breakend.bed
+    cat $GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.raw.bedpe | awk '$8>=3' > $GRIDSS_PON/${BATCH}/gridss_pon_breakpoint.bedpe
+    rm $GRIDSS_PON/${BATCH}/*raw*
+    
 
-    
-    
+
+
